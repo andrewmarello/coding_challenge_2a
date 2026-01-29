e287309536f4f211c8c07f44e60fed204f40bbc3
Andrew <andrewmarello@usf.edu>
2026-01-29 16:39:49 -0500

    Updated Names and costs for js file

--- challenge_2a.js
+++
@@ -1,6 +0,0 @@
-
-const name = "Andrew"
-console.log(`Hello ${name}.`)
-
-let number = 22
-console.log(`Your favorite number is ${number}.`)
---
+++ pricing_assistant_2a.js
@@ -0,0 +1,8 @@
+const name = "Andrew"
+console.log(`Hello ${name}. This is Coding Challenge 2a - Pricing Assistant.`)
+
+const product_name = "Running Shoes"
+let costPerUnit = 99.99
+let basePrice = 149.99
+let discountRate = 0.20
+let salesTaxRate = 0.07
